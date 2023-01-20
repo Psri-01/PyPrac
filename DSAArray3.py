c5c6bef5e0127e1fca522e7a775a426325ef8a71
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-20 15:42:16 +0530

    Create DSAArray3.py

---
+++ DSAArray3.py
@@ -0,0 +1,8 @@
+#Slicing python prog to reverse an array 
+def reverseList(A):
+  print(A[::-1])
+# Driver function to test above function
+A = [1,2,3,4,5,6]
+print(A)
+print("Reverse list is")
+reverseList(A)
