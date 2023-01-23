17eeb48b2295cbabf91dba3de6ccea32550962d1
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-23 23:46:24 +0530

    Update streplace.py

--- streplace.py
+++ streplace.py
@@ -1,3 +1,9 @@
 text="lions tigers n bears"
 rtext=text.replace("n", "and")
 print(rtext)
+#you can also do print(string.replace("n", "and")
+'''for repetitive words in string:
+string = "dieks for dieks dieks dieks dieks"
+
+to print the string by replacing only 3 occurrences of ek by a:
+print(string.replace("ek", "a", 3))'''
