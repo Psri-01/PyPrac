8a7347e8c3cfab747db563e0025696156c0e8bb3
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-24 00:18:18 +0530

    Update strmethods.py

--- strmethods.py
+++ strmethods.py
@@ -27,3 +27,8 @@ price = 7.5
 with_tax = price*1.09
 print("Base price: ${:.2f}. With tax: ${:.2f}".format(price, with_tax)) 
 # :.2f means we're going to format a float number and that there should be two digits after the decimal dot.
+
+def to_celsius(x):
+    return (x-32)*5/9
+for x in range(0,101,10):
+    print("{:>3} F | {:>6.2f} C".format(x,to_celsius(x)))
