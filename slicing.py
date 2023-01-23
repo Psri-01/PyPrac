3f198957d038c7ca040db213827f3b7ca4f100ea
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-23 22:43:51 +0530

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -6,3 +6,4 @@ print(brut[-1:]) #reverse index
 print(brut[:-3]) #from behind, leave 012 indices n print the rest
 print(brut[-6:-1])
 print(brut[2:7:2]) # here the last 2 is an index jump
+print(brut[:9]+brut[9:]) #to print the entire string
