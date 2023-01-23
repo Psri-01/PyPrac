fe47c6901ef1dea6c76adad2ae7b466442e588f1
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-23 22:40:40 +0530

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -1,8 +1,8 @@
 brut = "Hello, Pineapple"
 print(brut[:8])
 print(brut[11:])
-print(brut[3:10])
-print(brut[-1:])
-print(brut[:-3])
+print(brut[3:10]) #from index 3 to 10-1 i.e. 9
+print(brut[-1:]) #reverse index
+print(brut[:-3]) #from behind, leave 012 indices n print the rest
 print(brut[-6:-1])
-print(brut[2:7:2])# here the last 2 is an index jump
+print(brut[2:7:2]) # here the last 2 is an index jump
