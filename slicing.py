2c4892b5273a0d51125242eb257501668c95c39c
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-23 22:36:37 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,8 @@
+brut = "Hello, Pineapple"
+print(brut[:8])
+print(brut[11:])
+print(brut[3:10])
+print(brut[-1:])
+print(brut[:-3])
+print(brut[-6:-1])
+print(brut[2:7:2])# here the last 2 is an index jump
