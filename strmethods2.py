bf9d893cad27305cc779e19076b77ad0121e4689
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-24 21:50:45 +0530

    Create strmethods2.py

---
+++ strmethods2.py
@@ -0,0 +1,20 @@
+#rfind() method finds the last occurrence of the specified value. The rfind() method returns -1 if the value is not found.
+def replace_ending(sentence, old, new):
+	# Check if the old string is at the end of the sentence 
+	if sentence.endswith(old):
+		# Using i as the slicing index, combine the part
+		# of the sentence up to the matched string at the 
+		# end with the new string
+		i = sentence.rfind(old)
+		new_sentence = sentence[:i] + new
+		return new_sentence
+	return sentence
+	
+print(replace_ending("It's raining cats and cats", "cats", "dogs")) 
+# Should display "It's raining cats and dogs"
+print(replace_ending("She sells seashells by the seashore", "seashells", "donuts")) 
+# Should display "She sells seashells by the seashore"
+print(replace_ending("The weather is nice in May", "may", "april")) 
+# Should display "The weather is nice in May"
+print(replace_ending("The weather is nice in May", "May", "April")) 
+# Should display "The weather is nice in April"
