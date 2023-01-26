9fa028c4a407e370bdc005d666edee1b4793423c
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-26 23:35:31 +0530

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
+for beast,arms in cool_beasts.items():
+    print("{} have {}".format(beast, arms))
+    
+for ext in file_counts:
+  print(extension)
+
+for ext, amount in file_counts.items():
+  print("There are {} files with the .{} extension".format(amount, ext)
+        
+file_counts.keys()
+file_counts.values()
+        
+for value in file_counts.values():
+   print(value)
