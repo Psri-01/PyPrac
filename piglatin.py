4c2c16b2f9f29fb187a2fad618493445b432b6f3
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-26 22:27:20 +0530

    Update piglatin.py

--- piglatin.py
+++ piglatin.py
@@ -11,3 +11,14 @@ def pig_latin(text):
     
 print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
 print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+#ALITER
+def pig_latin(text):
+  say = ""
+  words = text.split()
+  for word in words:
+    texts = word[1:]+word[0]+'ay'+" "
+    say += texts
+  return say
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
