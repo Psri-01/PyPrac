925afcef11174f5085124578b9897f40200791de
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-27 15:49:24 +0530

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -19,3 +19,12 @@ new_items = {'jeans': ['white'], 'scarf': ['yellow'], 'socks': ['black', 'brown'
 wardrobe.update(new_items)
 #output:
 {'shirt': ['red', 'blue', 'white'], 'jeans': ['white'], 'scarf': ['yellow'], 'socks': ['black', 'brown']}
+
+# Generate a new list containing the elements of list2 followed by the elements of list1 in reverse order     
+def combine_lists(list1, list2):
+  
+  return list2 + list1[::-1]
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+
+print(combine_lists(Jamies_list, Drews_list))
