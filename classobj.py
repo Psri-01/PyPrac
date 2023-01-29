09d49cb53a50698fd47d0e129afc5e505237bfed
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-29 22:52:24 +0530

    Update classobj.py

--- classobj.py
+++ classobj.py
@@ -54,3 +54,15 @@ class Apple:
 jonagold = Apple("gold", "soft")
 print(jonagold.color)
 print(jonagold.flavor)
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def greeting(self):
+        # Should return "hi, my name is " followed by the name of the Person.
+        return "hi, my name is {}".format(self.name)
+
+# Create a new instance with a name of your choice
+some_person = Person("Grisha")
+# Call the greeting method
+print(some_person.greeting())
