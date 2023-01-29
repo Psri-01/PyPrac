0a1954b46d032dae55adf8075a31d9e6af0873b9
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-01-29 22:43:05 +0530

    Update classobj.py

--- classobj.py
+++ classobj.py
@@ -13,6 +13,24 @@ print("Roses are {},".format(rose.color))
 print("violets are {},".format(violet.color))
 print(this_pun_is_for_you) 
 
+class Piglet:
+  years = 0
+  def pig_years(self):
+    return self.years*18
+piggy = Piglet()
+print(piggy.pig_years())
+piggy.years = 2
+print(piggy.pig_years())
+
+#similar to piggy example
+class Dog:
+  years = 0
+  def dog_years(self):
+    return self.years*7
+fido = Dog()
+fido.years = 3
+print(fido.dog_years())
+
 class Apple:
   color = ""
   flavor = ""
@@ -28,3 +46,11 @@ golden.color = "gold"
 golden.flavor = "soft"
 print(golden.color)
 print(golden.flavor)
+
+class Apple:
+    def __init__(self, color, flavor):   #init is a constructor which takes an argument called self, assigning values to the variables.
+        self.color = color
+        self.flavor = flavor
+jonagold = Apple("gold", "soft")
+print(jonagold.color)
+print(jonagold.flavor)
