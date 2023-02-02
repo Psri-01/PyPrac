ee0567b1fd6713895da17d4a89b17296b7594b86
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-02-03 00:00:48 +0530

    Update class inheritance.py

--- class inheritance.py
+++ class inheritance.py
@@ -23,3 +23,14 @@ granny_smith=Apple("green","tart")
 print(granny_smith.flavor)
 carnelian=Grape("purple","sweet")
 print(carnelian.color)
+
+class Animal:
+	sound=""
+	def __init__(self,name):
+		self.name=name
+		def speak(self):
+			print("{sound} I'm {name}! {sound}".format{name=self.name,sound=self.sound})
+class Piglet(Animal):
+	sound="Oink!"
+hamlet=Piglet("Hamlet")
+hamlet.speak()
