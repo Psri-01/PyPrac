1ac74e040bc90f29efce2c4eb0a4024cd0a00a5e
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-02-12 16:34:53 +0530

    Update pandas1.py

--- pandas1.py
+++ pandas1.py
@@ -3,3 +3,4 @@ visitors = [1235, 6424, 9345, 8464, 2345]
 errors = [23, 45, 33, 44, 76]
 df = pandas.DataFrame({"visitors": visitors, "errors": errors}, index=["Mon", "Tue", "Wed", "Thu", "Fri"])
 print(df)
+df["errors"].mean()
