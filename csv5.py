5e8a6f2193183d9e1de7bce40f1b5329802c27cf
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-02-26 16:25:38 +0530

    Create csv5.py

---
+++ csv5.py
@@ -0,0 +1,25 @@
+#read
+import csv
+with open('names.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['first_name'], row['last_name'])
+
+'''Eric Idle
+John Cleese'''
+
+print(row)
+'''{'first_name': 'John', 'last_name': 'Cleese'}'''
+
+
+#write
+import csv
+
+with open('names.csv', 'w', newline='') as csvfile:
+    fieldnames = ['first_name', 'last_name']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
