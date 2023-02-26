6233b5ba828570b7eea94541348cf618681de3b8
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-02-26 18:19:45 +0530

    Create DSAArray10.py

---
+++ DSAArray10.py
@@ -0,0 +1,35 @@
+# Function to print the second largest element in an array
+def print2largest(arr, arr_size):
+
+	# There should be atleast two elements
+	if (arr_size < 2):
+	
+		print(" Invalid Input ")
+		return
+	
+
+	first = second = -2147483648
+	for i in range(arr_size):
+	
+		# If current element is smaller than first then update both first and second
+		if (arr[i] > first):
+		
+			second = first
+			first = arr[i]
+		
+
+		# If arr[i] is in between then update second
+		elif (arr[i] > second and arr[i] != first):
+			second = arr[i]
+	
+	if (second == -2147483648):
+		print("There is no second largest element")
+	else:
+		print("The second largest element is", second)
+
+
+# Driver code
+arr = [12, 35, 1, 10, 34, 1]
+n = len(arr)
+
+print2largest(arr, n)
