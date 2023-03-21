69c33e80cb2d21a33e1044aaae3340179e45e92f
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-03-21 23:16:33 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -11,3 +11,31 @@ def is_palindrome(input_string):
 		    
 print(is_palindrome("applppe"))
 print(is_palindrome("malayalam"))
+
+#ALITER
+return str(x)[::-1] == str(x)
+
+#ALITER
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        rev = 0
+        orig = x
+        while x != 0:
+            rev = rev * 10 + x % 10
+            x //= 10
+        return rev == orig
+
+#ALITER
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        half = 0
+        while x > half:
+            half = (half * 10) + (x % 10)
+            x = x // 10
+
+        return x == half or x == half // 10
