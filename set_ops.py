7867eb1b022e65aba181b14e21a548dc7b59f019
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-04-11 00:07:15 +0530

    Update and rename set_union.py to set_ops.py

--- set_ops.py
+++ set_ops.py
@@ -18,3 +18,11 @@ a = set(map(int,input().split()))
 input() #french
 b = set(map(int,input().split()))
 print(len(a.union(b)))
+
+#similarlt intersection
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+input()
+a = set(map(int,input().split()))
+input()
+b = set(map(int,input().split()))
+print(len(a.intersection(b)))
