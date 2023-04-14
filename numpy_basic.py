bb6b3571061c664fe2ee8e61f4a58236c4545875
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-04-14 21:40:36 +0530

    Update numpy_basic.py

--- numpy_basic.py
+++ numpy_basic.py
@@ -12,3 +12,23 @@ import numpy
 N = int(input())
 A = numpy.array([input().split() for _ in range(N)],float)
 print(round(numpy.linalg.det(A),2))
+#ALITER
+import numpy
+arr = []
+for _ in range(int(input())):
+    arr.append(list(map(float,input().split())))
+out = numpy.linalg.det(numpy.array(arr))
+#print("{0:.2f}".format(out))
+print(round(out,2))
+
+'''ARRAY(2 arrays A,B of dim N x M) MATH'''
+import numpy
+N, M = map(int, input().split())
+A = numpy.array([list(map(int, input().split())) for n in range(N)])
+B = numpy.array([list(map(int, input().split())) for n in range(N)])
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
+print(A ** B)
