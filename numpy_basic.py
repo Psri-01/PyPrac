2bfc9bd66ca822304e8ebb1d4d3803bb2f6d03a9
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-04-14 19:56:24 +0530

    Update numpy_basic.py

--- numpy_basic.py
+++ numpy_basic.py
@@ -5,3 +5,10 @@ import numpy
 L = list(map(float,input().split()))
 x = input();
 print (numpy.polyval(L, int(x))) #The polyval tool evaluates the polynomial at specific value.
+
+'''You are given a square matrix A with dimensions NxN. 
+Your task is to find the determinant. Note: Round the answer to 2 places after the decimal.'''
+import numpy
+N = int(input())
+A = numpy.array([input().split() for _ in range(N)],float)
+print(round(numpy.linalg.det(A),2))
