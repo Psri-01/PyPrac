845cebef4640011f6e9342cfd695b439f31eb4d5
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-04-15 12:29:37 +0530

    Update numpy_basic.py

--- numpy_basic.py
+++ numpy_basic.py
@@ -44,3 +44,9 @@ import numpy
 N,M = map(int,input().split())
 A = numpy.array([input().split() for _ in range(N)], int)
 print(numpy.prod(numpy.sum(A,axis=0),axis=0))
+
+'''MIN & MAX'''
+import numpy
+N,M = map(int,input().split())
+A = numpy.array([input().split()for _ in range(N)],int)
+print(numpy.max(numpy.min(A,axis=1),axis=0))
