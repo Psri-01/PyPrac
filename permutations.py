88627ddc2cf6744fef00d87a99fe3fc28d37fff3
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-01 11:47:16 +0530

    Create permutations.py

---
+++ permutations.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+seq=permutations(['1','2','3'])
+print(seq)
+for p in seq:
+    print(p)
+
+from itertools import permutations
+seq=permutations(['A','B'])
+for p in seq:
+    print(p)
+
+from itertools import permutations
+list1=['1','2','3','4']
+seq=permutations(list1)
+for p in seq:
+    print(p)
