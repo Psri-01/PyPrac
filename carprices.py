2d03a63e0a3901a513b1928cfe29a547dcdfe67c
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-06 00:17:59 +0530

    Delete carprices.py

--- carprices.py
+++
@@ -1,7 +0,0 @@
-def car_listing(car_prices):
-  result = ""
-  for cars, price in car_prices.items():
-    result += "{} costs {} dollars".format(cars, price) + "\n"
-  return result
-
-print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
