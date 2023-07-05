4fe5719599287c5f4ae8ba5176622fe054cdd155
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-05 10:37:13 +0530

    Delete animals.py

--- animals.py
+++
@@ -1,5 +0,0 @@
-animals = ["Lion","Zebra","Dolphin","Monkey"]
-chars = 0
-for animal in animals:
-  chars += len(animals)
-print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
