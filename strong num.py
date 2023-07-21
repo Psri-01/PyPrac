f65952d2ff1e9f65c85b664cf6cee199aa0cdb60
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-22 01:11:27 +0530

    Update strong num.py

--- strong num.py
+++ strong num.py
@@ -6,6 +6,24 @@ Explanation : Number = 145
 145 = 1! + 4! + 5!
 145 = 1 + 24 + 120'''
 
+#Using iteration
+n=145
+temp=n
+sum=0
+f=[0]*10
+f[0]=1
+f[1]=1
+for i in range(2,10):
+    f[i]=f[i-1]*i
+while(temp):
+    r=temp%10
+    temp=temp//10
+    sum+=f[r]
+if (sum==n):
+    print("Strong")
+else:
+    print("Not")
+    
 #Using Recursion
 def Factorial(num):
     if num<=0: return 1 else: return num*Factorial(num-1) sum=0 def check_StrongNumber(num): global sum if (num>0):
