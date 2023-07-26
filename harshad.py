7764ba85bebfc316b4015352927cfb4cbc1c5132
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-26 12:17:49 +0530

    Update harshad.py

--- harshad.py
+++ harshad.py
@@ -23,3 +23,14 @@ if(p%sum1==0):
     print('Harshad num')
 else:
     print('Not a hs num')
+
+n = 21
+p=n
+sum1=0
+while(n>0):
+    sum1+=n%10
+    n=n//10
+if(p%sum1==0):
+    print("Harshad number")
+else:
+    print("Not harshad number")
