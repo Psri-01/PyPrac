4e1f0523ce8fdc9e693de519de12815667fb34b9
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-27 15:34:19 +0530

    Create HCF.py

---
+++ HCF.py
@@ -0,0 +1,24 @@
+a=54
+b=24
+x=1
+for i in range(1,min(a+1,b+1)):
+    if a%i==0 and b%i==0:
+        hcf=i
+print("Hcf of",a,"and",b,"is",hcf)
+
+n1=36
+n2=60
+a=n1
+b=n2
+while n1!=n2:
+    if n1>n2:
+        n1-=n2
+    else:
+        n2-=n1
+print("Hcf of",a,"and",b,"is",n1)
+
+def getHcf(a,b):
+    return b==0 and a or getHcf(b,a%b)
+num1=36
+num2=60
+print("Hcf of", num1, "and", num2, "is", getHcf(num1, num2))
