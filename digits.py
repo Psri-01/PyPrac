67d046d769ac3730d98940dd5a45d7bc67555362
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-29 17:11:12 +0530

    Update digits.py

--- digits.py
+++ digits.py
@@ -11,3 +11,12 @@ print(digits(25))   # Should print 2
 print(digits(144))  # Should print 3
 print(digits(1000)) # Should print 4
 print(digits(0))    # Should print 1
+
+def countDigit(n):
+  digit=0
+  while n!=0:
+    n//=10
+    digit+=1
+  return digit
+n=78673
+print(countDigit(n))
