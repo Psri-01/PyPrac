d67c37a14676d83b014653b9905f8b3ba0d740ff
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-29 11:48:20 +0530

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -14,3 +14,18 @@ list1=['1','2','3','4']
 seq=permutations(list1)
 for p in seq:
     print(p)
+
+def factorial(num):
+    fact=1
+    for i in range(num,1,-1):
+        fact*=i
+    return fact
+n=10
+r=6
+p=factorial(n)//factorial(n-r)
+print(p)
+
+import math
+n = int(input(“Enter the number of Students:”))
+m = int(input(“Enter the number of Seats:”))
+print(math.perm(n, m))
