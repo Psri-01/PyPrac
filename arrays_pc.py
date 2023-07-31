3204313d1475db817b9eb4943b604435f2ab8292
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-31 17:13:37 +0530

    Update arrays_pc.py

--- arrays_pc.py
+++ arrays_pc.py
@@ -79,3 +79,12 @@ class Solution(object):
                 end=mid
             mid=(start+end)//2
         return start
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+        n=len(nums)
+        for i in range(1,n):
+            if nums[i]==nums[i-1]:
+                return True
+        return False
